699231ae364ade01fd575561dd5993bd36d00452
Arturo Garcia <arturogfin@gmail.com>
2025-09-24 16:54:51 -0300

    added t1

---
+++ t1/template/redblack.c
@@ -0,0 +1,175 @@
+#include "redblack.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+struct aluno* getAluno(){
+    struct aluno* retorno = malloc(sizeof(struct aluno));
+    if(!retorno)
+        matarProgramaFaltaMemoria();
+    retorno->nome = malloc(sizeof("SEU_NOME"));//sizeof conta o \0
+	if(!retorno->nome)
+		matarProgramaFaltaMemoria();
+    retorno->nomeDinf = malloc(sizeof("NOME_NAS_SERVIDORAS_DINF"));
+    if(!retorno->nomeDinf)
+        matarProgramaFaltaMemoria();
+    
+	strcpy(retorno->nome, "SEU_NOME");
+    strcpy(retorno->nomeDinf, "NOME_NAS_SERVIDORAS_DINF");
+    retorno->grr = 1234;
+
+	return retorno;
+}
+
+void imprimirDadosAluno(){
+    struct aluno* aluno = getAluno();
+    
+    printf("Trabalho de %s\n", aluno->nome);
+    printf("Login Dinf %s\n", aluno->nomeDinf);
+    printf("GRR %u\n\n", aluno->grr);
+
+    free(aluno->nome);
+    free(aluno->nomeDinf);
+    free(aluno);
+
+    return;
+}
+
+//IMPLEMENTE AS DEMAIS FUNÇÕES AQUI
+
+struct nodo *node_create(int chave)
+{
+    struct nodo *new_node = (struct nodo*)malloc(sizeof(struct nodo));
+    if (!new_node)
+    {
+        fprintf(stderr, "Falha ao alocar memoria\n");
+        exit(1);
+    }
+
+    new_node->chave = chave;
+    new_node->colour = RED;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->parent = NULL;
+
+    return new_node;
+}
+
+void rotation_left(struct nodo **raiz, struct nodo *x)
+{
+    struct nodo *sentinela = (*raiz)->parent;
+    struct nodo *y = x;
+
+    x->right = y->left;
+    if (y->left != sentinela)
+        y->left->parent = x;
+
+    y->parent = x->parent;
+    if (x->parent == sentinela)
+        (*raiz) = y;
+    else
+    {
+        if (x == x->parent->left)
+            x->parent->left = y;
+        else
+            x->parent->right = y;
+    }
+
+    y->left = x;
+    x->parent = y;
+}
+
+void rotation_right(struct nodo **raiz, struct nodo *x)
+{
+
+}
+
+void redblack_insert_fixup(struct nodo **raiz, struct nodo *z)
+{
+    while (z->parent->colour == RED)
+    {
+        if (z->parent == z->parent->parent->left)
+        {
+            struct nodo *y = z->parent->parent->right;
+            if (y->colour == RED)
+            {
+                z->parent->colour = BLACK;
+                y->colour = BLACK;
+                z->parent->parent->colour = RED;
+                z = z->parent->parent;
+            }
+            else
+            {
+                if (z == z->parent->right)
+                {
+                    z = z->parent;
+                    rotation_left(raiz, z);
+                }
+
+                z->parent->colour = BLACK;
+                z->parent->parent->colour = RED;
+                rotation_right(raiz, z->parent->parent);
+            }
+        }
+        else
+        {
+            //ESPELHO
+        }
+    }
+
+    (*raiz)->colour = BLACK;
+}
+
+struct nodo *inserir(struct nodo **raiz, int chave)
+{
+    if (*raiz == NULL)
+    {
+        struct nodo *sentinela = node_create(LIXO);
+        sentinela->colour = BLACK;
+
+        struct nodo *new_node = node_create(chave);
+        new_node->colour = BLACK;
+        new_node->left = sentinela;
+        new_node->right = sentinela;
+        new_node->parent = sentinela;
+        *raiz = new_node;
+
+        return *raiz;
+    }
+
+    struct nodo *sentinela = (*raiz)->parent;
+
+    if (buscar(raiz, chave) == sentinela)
+    {
+        printf("Falha ao inserir\n");
+        return *raiz;
+    }
+
+    struct nodo *z = node_create(chave);
+    struct nodo *x = *raiz;
+    struct nodo *y = sentinela;
+
+    while (x != sentinela)
+    {
+        y = x;
+        if (z->chave < x->chave)
+            x = x->left;
+        else
+            x = x->right;
+    }
+
+    z->parent = y;
+    if (y == sentinela)
+        (*raiz) = z;
+    else
+    {
+        if (z->chave < y->chave)
+            y->left = z;
+        else
+            y->right = z;
+    }
+
+    redblack_insert_fixup(raiz, z);
+    return (*raiz);
+}
---
+++ t1/template/redblack.h
@@ -0,0 +1,39 @@
+#ifndef REDBLACK_H_
+#define REDBLACK_H_
+
+#define BLACK 0
+#define RED 1
+#define LIXO -12345
+
+struct nodo {
+    int chave;
+    char colour;
+    struct nodo *left;
+    struct nodo *right;
+    struct nodo *parent;
+};
+
+struct aluno{
+    char* nome;//nome completo
+    char* nomeDinf;//nome de usuário nas servidoras do dinf
+    unsigned int grr;
+};
+
+struct aluno* getAluno();
+
+void imprimirDadosAluno();
+
+//retorna SENTINELA se não foi possível inserir
+struct nodo* inserir(struct nodo** raiz, int chave);
+
+// retorna o número de nodos excluídos
+int excluir(struct nodo** raiz, int chave);
+
+//retorna SENTINELA se não existe
+struct nodo* buscar(struct nodo* raiz, int chave);
+
+void imprimirEmOrdem(struct nodo* nodo);
+
+void imprimirEmLargura(struct nodo* raiz);
+
+#endif//REDBLACK_H
